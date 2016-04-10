ddec18f09eae291dd3fd816342be1a60d0466c3f
zhangqiangoffice <zhangqiangoffice@163.com>
2016-04-10 21:30:05 +0800

    初始化 及三个按钮的布局

---
+++ src/com/example/remotecontrol/RemoteControlActivity.java
@@ -0,0 +1,25 @@
+package com.example.remotecontrol;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+
+
+public class RemoteControlActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+    	return new RemoteControlFragment();
+    }
+    
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+		super.onCreate(savedInstanceState);
+    }
+
+   
+}
