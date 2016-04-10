ea26eab3377d0ffd8c1b62c7531e71809099fccf
zhangqiangoffice <zhangqiangoffice@163.com>
2016-04-10 22:10:15 +0800

    通过include实现布局复用

--- src/com/example/remotecontrol/RemoteControlFragment.java
+++ src/com/example/remotecontrol/RemoteControlFragment.java
@@ -1,5 +1,6 @@
 package com.example.remotecontrol;
 
+import android.R.integer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class RemoteControlFragment extends Fragment {
@@ -36,13 +39,35 @@ public class RemoteControlFragment extends Fragment {
 			}
 		};
 		
-		Button zeroButton = (Button) v.findViewById(R.id.fragment_remote_control_zeroButton);
+		
+		TableLayout tableLayout = (TableLayout) v.findViewById(R.id.fragment_remote_control_tableLayout);
+		int number = 1;
+		for (int i = 2; i < tableLayout.getChildCount() - 1; i++ ) {
+			TableRow row = (TableRow) tableLayout.getChildAt(i);
+			for (int j = 0; j < row.getChildCount(); j++) {
+				Button button = (Button) row.getChildAt(j);
+				button.setText("" + number);
+				button.setOnClickListener(numberButtonListener);
+				number++;
+			}
+		}
+		
+		TableRow bottomRow = (TableRow) tableLayout.getChildAt(tableLayout.getChildCount() - 1);
+		Button deleteButton = (Button) bottomRow.getChildAt(0);
+		deleteButton.setText("Delete");
+		deleteButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mWorkingTextView.setText("0");
+			}
+		});
+		
+		Button zeroButton = (Button) bottomRow.getChildAt(1);
+		zeroButton.setText("0");
 		zeroButton.setOnClickListener(numberButtonListener);
 		
-		Button oneButton = (Button) v.findViewById(R.id.fragment_remote_control_oneButton);
-		oneButton.setOnClickListener(numberButtonListener);
-		
-		Button enterButton = (Button) v.findViewById(R.id.fragment_remote_control_enterButton);
+		Button enterButton = (Button) bottomRow.getChildAt(2);
+		enterButton.setText("Enter");
 		enterButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
@@ -55,10 +80,6 @@ public class RemoteControlFragment extends Fragment {
 				
 			}
 		});
-		
 		return v;
 	}
-	
-	
-	
 }
